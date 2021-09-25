4067e2af8d4cb623b7bf5dc4aefc5f3662e4a02a
Douglas Lisboa <douglas.lisboa@edu.unipar.br>
2021-09-25 18:44:50 -0300

    Criado estrutura da entidade users

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,6 +1,24 @@
-import {Entity} from "typeorm";
+import {Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn} from "typeorm";
 
-@Entity()
-export class User {
+@Entity("users")
+class User {
+    @PrimaryColumn()
+    id: string;
 
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    admin: boolean;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @CreateDateColumn()
+    updated_at: Date;
 }
+
+export { User };
