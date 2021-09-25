b2eb0afaa7b1bdfb653452bb51149851992a7177
Douglas Lisboa <douglas.lisboa@edu.unipar.br>
2021-09-25 19:43:54 -0300

    Criado repositories; Criado interface service de usuario

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,9 +1,10 @@
 import {Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn} from "typeorm";
+import { v4 as uuid } from 'uuid';
 
 @Entity("users")
 class User {
     @PrimaryColumn()
-    id: string;
+    readonly id: string;
 
     @Column()
     name: string;
@@ -19,6 +20,12 @@ class User {
 
     @CreateDateColumn()
     updated_at: Date;
+
+    constructor() {
+        if(!this.id) {
+            this.id = uuid();
+        }
+    }
 }
 
 export { User };
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,13 @@
+interface IUserRequest {
+    name: string;
+    email: string;
+    admin?: boolean;
+}
+
+class CreateUserService {
+    async execute({ name, email, admin } : IUserRequest) {
+
+    }
+}
+
+export { CreateUserService }
